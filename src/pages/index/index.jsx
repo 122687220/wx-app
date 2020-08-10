12432e7f8a0481befcd058e14ed55009ecb18739
LAPTOP-TOIL3O72\lyl <122687220@qq.com>
2020-08-10 21:45:16 +0800

    first commit

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import { AtButton, AtTabBar } from 'taro-ui'
+
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.scss'
+
+export default class Index extends Component {
+
+  constructor () {
+    super(...arguments)
+    this.state = {
+      current: 0
+    }
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  handleClick (value) {
+    this.setState({
+      current: value
+    })
+  }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <AtTabBar
+          fixed
+          tabList={[
+            { title: '待办事项', iconType: 'bullet-list', text: 'new' },
+            { iconType: 'camera' },
+            { title: '文件夹', iconType: 'folder', text: '100', max: 99 }
+        ]}
+          onClick={this.handleClick.bind(this)}
+          current={this.state.current}
+        />
+      </View>
+    )
+  }
+}
